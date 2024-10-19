3fdc7456957cd3df3de8487f1950913d46718280
sasmalnirmalya <nirmalyasasmal2000@gmail.com>
2024-10-19 10:52:28 +0530

    Adding test cases

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,13 @@
 const {add } = require('../index');
 
-test('adds 1 + 2 to equal 3', () => {
+test('adds 1,2,3 to equal 6', () => {
     expect(add('1,2,3')).toBe(6);
 });
 
+test('empty string returns 0', () => {
+    expect(add('')).toBe(0);
+});
+
+test('1', () => {
+    expect(add('1')).toBe(1);
+});
